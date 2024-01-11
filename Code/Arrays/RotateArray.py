28ac2c93c266cf4af608b87c8d7ab54263e9eba0
SindhuNamana <114772845+SindhuNamana@users.noreply.github.com>
2024-01-11 00:42:30 -0500

    Update RotateArray.py

--- Code/Arrays/RotateArray.py
+++ Code/Arrays/RotateArray.py
@@ -1,6 +1,15 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
 
+        #Brute force
+        for i in range(k):
+            for j in range(len(nums)):
+                nums[j], nums[-1] = nums[-1], nums[j]
+        return nums
+        
+        #Time Complexity = O(n*k)
+        #Space Complexity = O(1)
+#-----------------------------------------------------------------------------------
         l = len(nums)
         nums_new = [0]*l
         for i in range(l):
