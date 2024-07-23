90f18674a460190fe3cbd4ade27a7ac5487890d8
SindhuNamana <114772845+SindhuNamana@users.noreply.github.com>
2024-07-23 14:17:33 -0400

    Update Min Cost Climbing Stairs.py

--- Code/Dynamic Programming/Min Cost Climbing Stairs.py
+++ Code/Dynamic Programming/Min Cost Climbing Stairs.py
@@ -8,3 +8,22 @@ class Solution:
 
         #Time Complexity = O(N)
         #Space Complexity = O(N)
+
+
+#Top-Down Approach
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        def mincost(i):
+            if i<=1:
+                return 0
+                
+            if i in memo:
+                return memo[i]
+    
+            memo[i] = min(mincost(i-1)+cost[i-1], mincost(i-2)+cost[i-2])
+            return memo[i]
+        memo = {}
+        return mincost(len(cost))
+
+        #Time Complexity = O(N)
+        #Space Complexity = O(N)
