425d391387ab1e26b5597c3ecdc6658db55b321c
SindhuNamana <114772845+SindhuNamana@users.noreply.github.com>
2024-07-26 16:41:13 -0400

    Update MinimumSumPath.py

--- Code/Dynamic Programming/MinimumSumPath.py
+++ Code/Dynamic Programming/MinimumSumPath.py
@@ -14,3 +14,25 @@ class Solution:
                     dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j]
 
         return dp[-1][-1]
+        #Time Complexity = O(mn)
+        #Space Complexity = O(mn)
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        dp = [0]*len(grid[0])
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i==0 and j==0:
+                    dp[j] = grid[i][j]
+                elif i==0 and j!=0:
+                    dp[j] = dp[j-1]+grid[i][j]
+                elif j==0 and i!=0:
+                    dp[j]= dp[j]+grid[i][j]
+                else:
+                    dp[j]=min(dp[j],dp[j-1])+grid[i][j]
+
+        return dp[-1]
+        #Time Complexity = O(mn)
+        #Space Complexity = O(n)
